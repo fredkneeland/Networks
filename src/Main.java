5a968306fb324ed05800578328902d311b81d6ab
fredkneeland <fred@bladelinux.com>
2015-10-06 20:33:10 -0600

    deleted testing output

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
-        int numb = 12;
-        byte a = (byte) numb;
-        System.out.println("Byte: " + a);
-
         runServer(args);
         //runClient(args);
     }
